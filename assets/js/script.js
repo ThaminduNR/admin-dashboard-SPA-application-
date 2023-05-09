b3a82b431000c43f50e51629fcbfbd4b6ebe24d3
ThaminduNR <thaminduniwantha777@gmail.com>
2023-05-09 23:47:25 +0530

    spa dashboard

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$(".toggle-btn").click(() => {
+  $(".side-bar").css({ left: "0" });
+  $(".toggle-btn").css({ display: "none" });
+  $("#toggle-btn-close").css({ display: "block" });
+});
+
+$("#toggle-btn-close").click(() => {
+  $(".side-bar").css({ left: "-250px" });
+  $(".toggle-btn").css({ display: "block" });
+  $("#toggle-btn-close").css({ display: "none" });
+});
+
+$("#customer").click(() => {
+  console.log(event);
+  $("#customerForm").css({ display: "block" });
+  $("#productForm").css({ display: "none" });
+  $("#placeOrderForm").css({ display: "none" });
+});
+$("#product").click(() => {
+  console.log("Hello click");
+  $("#customerForm").css({ display: "none" });
+  $("#productForm").css({ display: "block" });
+  $("#placeOrderForm").css({ display: "none" });
+});
+$("#placeOrder").click(() => {
+  $("#customerForm").css({ display: "none" });
+  $("#productForm").css({ display: "none" });
+  $("#placeOrderForm").css({ display: "block" });
+});
